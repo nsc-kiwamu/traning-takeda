7cf73affafa503c85b242d3d6d215127a50cacf7
TakedaAyano <a.takeda@n-s-c.co.jp>
2022-07-05 13:07:33 +0900

    課題03作成

--- src/traning04/Practice04.ts
+++ src/traning04/Practice04.ts
@@ -7,7 +7,25 @@
  ・条件
  　number型の配列は通常のfor文でループさせる
  　string型の配列はfor-of文でループさせる
+*/
 
+// 配列の中身を表示 for文でのループ(number)
+const arraySampleNum: number[] = [1,2,3,4,5,6,7,8,9,10]
+for (let i = 0; i < arraySampleNum.length; i++){
+    console.log(arraySampleNum[i])
+}
+
+
+// 配列の中身を表示 通常のfor-of文(string)
+const arraySampleStr: string[] = ['りんご','いちご','りんご','ぶどう','もも','りんご','いちご','もも']
+for (const str of arraySampleStr) {
+    console.log(str)
+}
+
+// ここまでは完成
+
+
+/*
  <チャレンジ問題>
  string型の配列('りんご','いちご','りんご','ぶどう','もも','りんご','いちご','もも')を
  for文でループし、以下の結果が出力されるように作成すること
@@ -16,4 +34,16 @@
  ぶどう:1個
  もも:2個
  すいか:0個
+ */
+
+ // 調べてもどのように書けばいいかわかりませんでした
+ // 配列の中身を表示 for文でのループ(string)
+const chaSampleNum: string[] = ['りんご','いちご','りんご','ぶどう','もも','りんご','いちご','もも']
+for (let i = 0; i < chaSampleNum.length; i++){
+    console.log()
+}
+
+/*※1 「Ctrl + @」でターミナルを開き以下を実行
+  yarn tsc 
+  node dist/traning04/Practice04.js
  */
