6ac90dfcce875df7aaa86edfe1d8eb2363c40c3f
nsc-kiwamu <kiwamu@n-s-c.co.jp>
2022-07-11 23:26:21 +0900

    課題09追加

---
+++ src/traning09/Practice09.ts
@@ -0,0 +1,19 @@
+/*
+ type型と繰り返し
+
+  <実施内容>
+  1. サンプルを参考にCompanyEmployee型のuserXを一人作り、以下をconsoleに表示させる
+  　竹田さんは23歳の女性です
+
+  2. サンプルを参考にCompanyEmployee型のuserYとuserZを作りfor文でループさせる
+  3. Common09を参考に以下の要素を持つ、typeであるFruitを作成する
+     type名：Fruit
+     要素：name:string、color:string
+
+     ちなみに、作成済のCompanyEmployeeは以下である
+     type名：CompanyEmployee
+     要素：name: string、 age: number、gender: string
+
+   4.Fruit型の変数appleを作成し、以下をconsoleに表示させる
+   　りんごは赤いです
+*/
