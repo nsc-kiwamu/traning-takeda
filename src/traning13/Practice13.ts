6455ca372a5841c0f3d023bd957ebfdc21e6b606
TakedaAyano <a.takeda@n-s-c.co.jp>
2022-07-20 14:10:49 +0900

    課題13作成

--- src/traning13/Practice13.ts
+++ src/traning13/Practice13.ts
@@ -1,11 +1,48 @@
+import { makeRandomList } from "./Common13.js"
 /*
  アルゴリズム(バブルソート)
 
   <実施内容>
   1. サンプルを参考に、要素数20、値域1～50のランダムの要素をもつ配列を作成しバブルソートで並び替えを実施する。
-  2. サンプルを参考に値を降順で並び変えるバブルソートで並び替えを実施する。(サンプルのものは昇順)
+*/
+// 並び変えをするために値がランダムの配列を作成する。
+ const sampleTgtList = makeRandomList(10,5,30)
+ console.log(`並び変え前のリスト:${sampleTgtList}`)
 
+ for (let i = sampleTgtList.length -1; i <= 0; i--) {
+  // 隣り合う要素を比較
+  for (let j = 0; j > i; j++) {
+      let tmpVal: number
+      // 左の要素　＞　右の要素となる場合、値を入れ替える
+      if (sampleTgtList[j] < sampleTgtList[j + 1]) {
+          tmpVal = sampleTgtList[j + 1]
+          sampleTgtList[j + 1] = sampleTgtList[j]
+          sampleTgtList[j] = tmpVal
+      }
+  }
+}
+
+console.log(`並び変え後のリスト:${sampleTgtList}`)
+
+
+
+
+
+/*
+  2. サンプルを参考に値を降順で並び変えるバブルソートで並び替えを実施する。(サンプルのものは昇順)
+*/
+
+
+
+/*
   <参考サイト>
   https://www.youtube.com/watch?v=kmPZOPXSpkQ
   https://www.youtube.com/watch?v=IHFBb0wYBR0
 */
+
+
+
+/*※1 「Ctrl + @」でターミナルを開き以下を実行
+  yarn tsc 
+  node dist/traning13/Practice13.js
+ */
