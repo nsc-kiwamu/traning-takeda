1336171996af55addb9919150fe58981eac59e49
nsc-kiwamu <kiwamu@n-s-c.co.jp>
2022-07-27 23:04:07 +0900

    バブルソートのコメント追加

--- src/traning13/Practice13.ts
+++ src/traning13/Practice13.ts
@@ -10,17 +10,20 @@ const sampleTgtList = makeRandomList(20,1,50)
 console.log(`並び変え前のリスト:${sampleTgtList}`)
 
 for (let i = sampleTgtList.length -1; i >= 0; i--) { // 20から1つずつ数を減らしながら、出力
+  console.log(`----------------- 比較対象の最終index定義(i)=${i}`)
   // 隣り合う要素を比較
   for (let j = 0; j < i; j++) { // このjとはなんでしょうか？隣り合ったiの値とjの値を比べるためのものでしょうか？
+    console.log(`隣り合う値の比較 j=${j} j番目の値=${sampleTgtList[j]} j+1番目の値=${sampleTgtList[j+1]}`)
       let tmpVal: number
       // 左の要素　＞　右の要素となる場合、値を入れ替える
       if (sampleTgtList[j] > sampleTgtList[j + 1]) {  // 
+        console.log('値を入れ替えました')
           tmpVal = sampleTgtList[j + 1]
           sampleTgtList[j + 1] = sampleTgtList[j]
           sampleTgtList[j] = tmpVal
       }
-     // console.log('テスト(j)', (j))
-     // console.log('テスト２(i)', (i))
+    //  console.log('テスト(j)', (j))
+    //  console.log('テスト２(i)', (i))
   }
 }
 
