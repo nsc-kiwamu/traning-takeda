3fd0816f246cf7cd657e727e025b33aa4683a29d
TakedaAyano <a.takeda@n-s-c.co.jp>
2022-08-02 17:40:29 +0900

    課題06復習

--- src/traning06/Practice06.ts
+++ src/traning06/Practice06.ts
@@ -6,8 +6,52 @@
  設定した値をfor文でループさせながらターミナルに出力する。
  */
 
-
+ // 4つのMapを設定
+ const sampleMap01 = new Map<string, string>()
+ const sampleMap02 = new Map<string, string>()
+ const sampleMap03 = new Map<string, string>()
+ const sampleMap04 = new Map<string, string>()
  
+ // Map01の値の設定
+ sampleMap01.set("いちご", "200円")
+ sampleMap01.set("ぶどう", "300円")
+ sampleMap01.set("りんご", "400円")
+
+ // Map02の値の設定
+ sampleMap02.set("ライオン", "ネコ科")
+ sampleMap02.set("犬", "イヌ科")
+ sampleMap02.set("猫", "ネコ科")
+
+ // Map03の値の設定
+ sampleMap03.set("今日の天気", "晴れ")
+ sampleMap03.set("明日の天気", "くもり")
+ sampleMap03.set("明後日の天気", "雨")
+
+ // Map04の値の設定
+ sampleMap04.set("私の父", "54歳")
+ sampleMap04.set("私の母", "51歳歳")
+ sampleMap04.set("私", "23歳")
+
+ // Map01の出力
+ for (const [key, value] of sampleMap01) {
+   console.log(`${key}は${value}です`)
+ }
+
+ // Map02の出力
+ for (const [key, value] of sampleMap02) {
+  console.log(`${key}は${value}です`)
+}
+
+// Map03の出力
+for (const [key, value] of sampleMap03) {
+  console.log(`${key}は${value}です`)
+}
+
+// Map04の出力
+for (const [key, value] of sampleMap04) {
+  console.log(`${key}は${value}です`)
+}
+
 
  /*※1 「Ctrl + @」でターミナルを開き以下を実行
    yarn tsc 
