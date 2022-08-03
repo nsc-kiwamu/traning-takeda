84c4d10a5d8e70912c08a48f75d1bc3ecb8ac99c
TakedaAyano <a.takeda@n-s-c.co.jp>
2022-08-03 16:57:20 +0900

    課題09復習

--- src/traning09/Practice09.ts
+++ src/traning09/Practice09.ts
@@ -5,7 +5,17 @@
   1. サンプルを参考にCompanyEmployee型のuserXを一人作り、以下をconsoleに表示させる
   　竹田さんは23歳の女性です
 */
+ const userX:CompanyEmployee = {name:'竹田',age:23, gender:'女性'}
+ console.log(`${userX.name}さんは${userX.age}歳の${userX.gender}です`)
+
  // 2. サンプルを参考にCompanyEmployee型のuserYとuserZを作りfor文でループさせる
+ const userY:CompanyEmployee = {name:'白井',age:45, gender:'男性'}
+ const userZ:CompanyEmployee = {name:'今埜',age:24, gender:'女性'}
+
+ const sampleList:CompanyEmployee[] = [userY, userZ]
+ for (const user of sampleList) {
+     console.log(`${user.name}さんは${user.age}歳の${user.gender}です`)
+ }
 
 /* 3. Common09を参考に以下の要素を持つ、typeであるFruitを作成する
      type名：Fruit
@@ -14,10 +24,15 @@
      ちなみに、作成済のCompanyEmployeeは以下である
      type名：CompanyEmployee
      要素：name: string、 age: number、gender: string
-*/
-/*   4.Fruit型の変数appleを作成し、以下をconsoleに表示させる
+
+     4.Fruit型の変数appleを作成し、以下をconsoleに表示させる
    　りんごは赤いです
 */
+ const sampleFruitList:Fruit = {name:'りんご',color:'赤い'}
+ console.log(`${sampleFruitList.name}は${sampleFruitList.color}です`)
+
+
+
 
  /*※1 「Ctrl + @」でターミナルを開き以下を実行
   yarn tsc 
