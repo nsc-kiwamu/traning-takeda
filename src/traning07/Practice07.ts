fe8e5f52f1b25aa9496fb8808fd7a11dcb71d59b
TakedaAyano <a.takeda@n-s-c.co.jp>
2022-08-03 14:50:16 +0900

    課題07復習

--- src/traning07/Practice07.ts
+++ src/traning07/Practice07.ts
@@ -14,7 +14,34 @@
 
 */
 
+// 所属を管理するMAP
+const sampleMap = new Map<string, string>([
+  ['白井', '第二システム統括部'],
+  ['佐藤', '第二システム統括部システム第1部'],
+  ['清野', '第二システム統括部システム第1部1課'],
+  ['遠藤', '第二システム統括部システム第2部'],
+  ['永池', '第二システム統括部システム第3部'],
+  ['竹田', '第二システム統括部システム第1部1課'],
+  ['吉田', 'システム事業本部']
+])
 
+// 所属を確認したいメンバー一覧
+const sampleMapList: Array<string> = ['白井','遠藤','佐藤', '竹田','清野','永池','吉田']
+
+// 所属の確認
+for (const member of sampleMapList) {
+  console.log(`所属を確認したいのは${member}さんです`)
+
+  // mapから所属を取得する
+  const div = sampleMap.get(member)
+
+  // 所属の取得有無でメッセージを変更する
+  if (div !== undefined) {
+      console.log(`>>>${member}さんの所属は${div}です`)
+  } else {
+      console.log(`>>>${member}さんは所属が見つかりませんでした`)
+  }
+}
 
 /*※1 「Ctrl + @」でターミナルを開き以下を実行
   yarn tsc 
