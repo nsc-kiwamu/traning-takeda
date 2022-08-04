b78d3da9dc542c523b4eca102136cebabd9b5357
TakedaAyano <a.takeda@n-s-c.co.jp>
2022-08-04 14:03:04 +0900

    課題10復習

--- src/traning10/Practice10.ts
+++ src/traning10/Practice10.ts
@@ -1,4 +1,4 @@
-import { formatDate, selectDogType, Animal, selectAnyType } from "./Common10.js";
+import { formatDate, selectDogType, Animal, selectAnyType, filterType } from "./Common10.js";
 
 /*
  色々な型と繰り返し
@@ -6,18 +6,32 @@ import { formatDate, selectDogType, Animal, selectAnyType } from "./Common10.js"
   <実施内容>結果は以下となるように引数を設定すること .OK
   　2022年7月12日
 */
+ const year = 2022
+ const month = 7
+ const day = 12
+ formatDate(year,month, day)
+ 
 
 /*
   2. formatDateを実行し、その結果をコンソールに出力する。結果は以下となるように引数を設定すること .OK
   　月の指定が不正です:XX 1～12を指定して下さい
   　※XXは任意の値で良い
 */
+ const year2 = 2022
+ const month2 = 15
+ const day2 = 4
+ formatDate(year2,month2,day2)
+
 
 /*
   3. formatDateを実行し、その結果をコンソールに出力する。結果は以下となるように引数を設定すること .OK
   　日の指定が不正です:XX 1～31を指定して下さい
   　※XXは任意の値で良い
 */
+ const year3 = 2022
+ const month3 = 8
+ const day3 = 40
+ formatDate(year3,month3,day3)
 
 
 /*
@@ -26,7 +40,13 @@ import { formatDate, selectDogType, Animal, selectAnyType } from "./Common10.js"
   　作成した配列をselectDogTypeの引数として渡し実行する
   　実行した結果をコンソールに出力する
 */
-
+ const animals: Animal[] = [{name: 'ぺんぎん', type: 'bird'},{name: 'ライオン', type: 'cat'},{name: '犬', type: 'dog'}]
+ console.log('犬科の動物を探します')
+ const dogs: string[] = selectDogType(animals)
+ for (let dog of dogs) {
+     console.log(dog)
+ }
+ console.log('犬科の動物は以上です。\r\n')
 
 
 /*
@@ -41,15 +61,38 @@ import { formatDate, selectDogType, Animal, selectAnyType } from "./Common10.js"
   　　サンプルの実行結果と同じになること
 */
 
+ const animals2: Animal[] = [{name: 'ぺんぎん', type: 'bird'},{name: 'ライオン', type: 'cat'},{name: '猫', type: 'cat'}]
+ const type = 'cat'
+ console.log('猫科の動物を探します')
+ const cats: string[] = selectAnyType(animals2,type)
+ for (let cat of cats) {
+    console.log(cat)
+ }
+ console.log('猫科の動物は以上です。\r\n')
+
+
 /*
   7. 作成したselectAnyTypeについて、以下の引数を渡して実行する
   　　第一引数は4で使用していた配列と同じもの
   　　第二引数は'dog'
   　　課題4の実行結果と同じになること
 */
+ const animals3: Animal[] = [{name: 'ぺんぎん', type: 'bird'},{name: 'ライオン', type: 'cat'},{name: '犬', type: 'dog'}]
+ const type2 = 'dog'
+ console.log('犬科の動物を探します')
+ const dogs2: string[] = selectAnyType(animals3,type2)
+ for (let dog of dogs2) {
+    console.log(dog)
+ }
+ console.log('犬科の動物は以上です。\r\n')
+
+
 /*
   8. filterTypeの内容を確認し、処理内容を理解する
 */
+
+
+
 
 /*※1 「Ctrl + @」でターミナルを開き以下を実行
   yarn tsc 
