8b1db09ae1dcdc2a88e74fdf6d97e57090beb826
TakedaAyano <a.takeda@n-s-c.co.jp>
2022-08-08 11:48:39 +0900

    課題12復習

--- src/traning12/Practice12.ts
+++ src/traning12/Practice12.ts
@@ -20,11 +20,11 @@ import { dice, gambler, card} from "./Common12.js";
  }
  // サイコロを振って結果で勝負する
  if (users[0].matchValue > users[1].matchValue) {
-     console.log(`${users[0].name}さんの勝ちです`)
+     console.log(`${users[0].name}さんの勝ちです\r\n`)
  } else if (users[0].matchValue < users[1].matchValue) {
-     console.log(`${users[1].name}さんの勝ちです`)
+     console.log(`${users[1].name}さんの勝ちです\r\n`)
  } else {
-     console.log('引き分けです')
+     console.log('引き分けです\r\n')
  }
 
 
@@ -34,6 +34,27 @@ import { dice, gambler, card} from "./Common12.js";
   　勝負方法:サイコロを2回振って合計が大きな数値の出た方の勝ち
   　勝負結果:「～さんの勝ちです」か「引き分けです」を出力する
 */
+// 対戦者の作成
+const users2:gambler[] = [{name:'白井', coin:0, matchValue:[1,2]}, {name:'遠藤', coin:0, matchValue:[1,2]}]
+// サイコロを振って結果を格納する
+for (let user of users2) {
+    // サイコロを振る
+    const value = dice()
+    const value2 = dice()
+    console.log(`${user.name}さんがサイコロを振った結果は${value + value2}です`)
+    // 結果を格納する
+    user.matchValue = [value + value2]
+}
+// サイコロを振って結果で勝負する
+if (users2[0].matchValue[0] > users2[1].matchValue[0]) {
+    console.log(`${users2[0].name}さんの勝ちです\r\n`)
+} else if (users2[0].matchValue[0] < users2[1].matchValue[0]) {
+    console.log(`${users2[1].name}さんの勝ちです\r\n`)
+} 
+else {
+    console.log('引き分けです\r\n')
+}
+
 
 
 /*
@@ -46,6 +67,68 @@ import { dice, gambler, card} from "./Common12.js";
   　勝負結果:「～さんの勝ちです」か「引き分けです」を出力する
 */
 
+const users3:gambler[] = [{name:'鈴木', coin:0, matchValue:[1,2,3]}, {name:'上野', coin:0, matchValue:[1,2,3]}] 
+// サイコロを振って結果を格納する
+for (let user of users3) {
+    // サイコロを振る
+    const value = dice()
+    const value2 = dice()
+    const value3 = dice()
+    console.log(`${user.name}さんのサイコロの結果、1回目:${value} 2回目:${value2} 3回目:${value3}`)
+    // 結果を格納する
+    user.matchValue = [value,value2,value3]
+}
+
+if (users3[0].matchValue[0] > users3[1].matchValue[0] &&
+    users3[0].matchValue[1] > users3[1].matchValue[1]){
+      console.log(`${users3[0].name}さんの勝ちです\r\n`)
+} else if (users3[0].matchValue[0] > users3[1].matchValue[0] &&
+           users3[0].matchValue[2] > users3[1].matchValue[2]){
+      console.log(`${users3[0].name}さんの勝ちです\r\n`)
+} else if (users3[0].matchValue[1] > users3[1].matchValue[1] &&
+           users3[0].matchValue[2] > users3[1].matchValue[2]){
+      console.log(`${users3[0].name}さんの勝ちです\r\n`)
+} else if (users3[0].matchValue[0] == users3[1].matchValue[0] &&
+           users3[0].matchValue[1] == users3[1].matchValue[1] &&
+           users3[0].matchValue[2] > users3[1].matchValue[2]){
+      console.log(`${users3[0].name}さんの勝ちです\r\n`)
+} else if (users3[0].matchValue[0] == users3[1].matchValue[0] &&
+           users3[0].matchValue[1] > users3[1].matchValue[1] &&
+           users3[0].matchValue[2] == users3[1].matchValue[2]){
+      console.log(`${users3[0].name}さんの勝ちです\r\n`)
+} else if (users3[0].matchValue[0] > users3[1].matchValue[0] &&
+           users3[0].matchValue[1] == users3[1].matchValue[1] &&
+           users3[0].matchValue[2] == users3[1].matchValue[2]){
+      console.log(`${users3[0].name}さんの勝ちです\r\n`)
+} else if (users3[0].matchValue[0] < users3[1].matchValue[0] &&
+           users3[0].matchValue[1] < users3[1].matchValue[1] ){
+      console.log(`${users3[1].name}さんの勝ちです\r\n`)
+} else if (users3[0].matchValue[0] < users3[1].matchValue[0] &&
+           users3[0].matchValue[2] < users3[1].matchValue[2]){
+      console.log(`${users3[1].name}さんの勝ちです\r\n`)                                       
+} else if (users3[0].matchValue[1] < users3[1].matchValue[1] &&
+           users3[0].matchValue[2] < users3[1].matchValue[2]){
+      console.log(`${users3[1].name}さんの勝ちです\r\n`)
+}  else if (users3[0].matchValue[0] == users3[1].matchValue[0] &&
+           users3[0].matchValue[1] == users3[1].matchValue[1] &&
+           users3[0].matchValue[2] < users3[1].matchValue[2]){
+      console.log(`${users3[1].name}さんの勝ちです\r\n`)
+} else if (users3[0].matchValue[0] == users3[1].matchValue[0] &&
+           users3[0].matchValue[1] < users3[1].matchValue[1] &&
+           users3[0].matchValue[2] == users3[1].matchValue[2]){
+      console.log(`${users3[1].name}さんの勝ちです\r\n`)
+} else if (users3[0].matchValue[0] < users3[1].matchValue[0] &&
+           users3[0].matchValue[1] == users3[1].matchValue[1] &&
+           users3[0].matchValue[2] == users3[1].matchValue[2]){
+      console.log(`${users3[1].name}さんの勝ちです\r\n`)
+} 
+
+else {
+  console.log('引き分けです\r\n')
+}
+
+
+
 /*
   4. サンプルを参考に以下の条件を満たすように呼び出しをすること。
   　対戦者:4名
@@ -54,6 +137,27 @@ import { dice, gambler, card} from "./Common12.js";
   　　例)Aさん:1、Bさん:1、Cさん:5、Dさん5 →　「Cさんの勝ちです」「Dさんの勝ちです」と出すか「CさんとDさんの勝ちです」と出す
   　　　　全員が同じ値になった場合のみ「引き分けです」を出す
 */
+const users4:gambler[] = [{name:'鈴木', coin:0, matchValue:[0]}, {name:'上野', coin:0, matchValue:[0]},
+                          {name:'田中', coin:0, matchValue:[0]}, {name:'岡田', coin:0, matchValue:[0]}] 
+
+for (let user of users4) {
+  // 1回だけ振る
+  const value = dice()
+  // 結果の表示
+  console.log(`${user.name}さんのサイコロの結果は、${value}です。`)
+  // 結果の格納
+  user.matchValue = [value]
+}
+
+ // 引き分けの処理
+if () {
+  console.log(`引き分けです\r\n`)
+
+ // 勝ち負けを判定する
+for (){
+
+}
+}
 
 /*
   5. サンプルを参考に以下の条件を満たすように呼び出しをすること。.OK
@@ -62,6 +166,27 @@ import { dice, gambler, card} from "./Common12.js";
   　勝負結果:「～さんの勝ちです」か「引き分けです」を出力する
   　※関数diceを参考に1-13の値をランダムで返却する関数cardを作成し、使用すること
 */
+
+// 対戦者の作成
+const users5:gambler[] = [{name:'白井', coin:0, matchValue:[0]}, {name:'遠藤', coin:0, matchValue:[0]}]
+// サイコロを振って結果を格納する
+for (let user of users5) {
+    // サイコロを振る
+    const value = card()
+    console.log(`${user.name}さんがカードをめくった結果は${value}です`)
+    // 結果を格納する
+    user.matchValue = [value]
+}
+// サイコロを振って結果で勝負する
+if (users5[0].matchValue[0] > users5[1].matchValue[0]) {
+    console.log(`${users5[0].name}さんの勝ちです\r\n`)
+} else if (users5[0].matchValue[0] < users5[1].matchValue[0]) {
+    console.log(`${users5[1].name}さんの勝ちです\r\n`)
+} else {
+    console.log('引き分けです\r\n')
+}
+
+
 
 /*※1 「Ctrl + @」でターミナルを開き以下を実行
   yarn tsc 
