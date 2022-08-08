7c39e3395ccb97b4725d50a04aa93ea0dc4b7936
TakedaAyano <a.takeda@n-s-c.co.jp>
2022-08-08 11:40:25 +0900

    課題13復習

--- src/traning13/Practice13.ts
+++ src/traning13/Practice13.ts
@@ -7,9 +7,52 @@ import { makeRandomList } from "./Common13.js"
 */
 // 並び変えをするために値がランダムの配列を作成する。
 
+const sampleTgtList = makeRandomList(20,1,50)
+console.log(`並び変え前のリスト:${sampleTgtList}`)
+
+for (let i = sampleTgtList.length -1; i >= 0; i--) {
+  // 隣り合う要素を比較
+  for (let j = 0; j < i; j++) {
+      let tmpVal: number
+      // 左の要素　＞　右の要素となる場合、値を入れ替える
+      if (sampleTgtList[j] > sampleTgtList[j + 1]) {
+        // 右の要素をtmpvalに代入
+          tmpVal = sampleTgtList[j + 1]
+        // 右の要素に左の要素を代入する
+          sampleTgtList[j + 1] = sampleTgtList[j]
+        // 右の要素を代入したtmpvalに左の要素を代入する
+          sampleTgtList[j] = tmpVal
+      }
+  }
+}
+
+console.log(`並び変え後のリスト:${sampleTgtList}\r\n`)
+
+
 /*
   2. サンプルを参考に値を降順で並び変えるバブルソートで並び替えを実施する。(サンプルのものは昇順)
 */
+
+const sampleTgtList2 = makeRandomList(20,1,50)
+console.log(`並び変え前のリスト:${sampleTgtList2}`)
+
+for (let i = sampleTgtList2.length -1; i >= 0; i--) {
+  // 隣り合う要素を比較
+  for (let j = 0; j < i; j++) {
+      let tmpVal: number
+      // 左の要素　＜　右の要素となる場合、値を入れ替える
+      if (sampleTgtList2[j] < sampleTgtList2[j + 1]) {
+        // 右の要素をtmpvalに代入
+          tmpVal = sampleTgtList2[j + 1]
+        // 右の要素に左の要素を代入する
+          sampleTgtList2[j + 1] = sampleTgtList2[j]
+        // 左の要素に右の要素を代入したtmpvalを代入する
+          sampleTgtList2[j] = tmpVal
+      }
+  }
+}
+
+console.log(`並び変え後のリスト:${sampleTgtList2}\r\n`)
 
 /*
   <参考サイト>
