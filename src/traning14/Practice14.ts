96709f7399279d93b0d615934ef7e159b484a9a2
TakedaAyano <a.takeda@n-s-c.co.jp>
2022-08-18 17:39:02 +0900

    課題14作成

--- src/traning14/Practice14.ts
+++ src/traning14/Practice14.ts
@@ -8,13 +8,15 @@ import { makeRandomList } from "../traning13/Common13";
 // https://noumenon-th.net/programming/2018/04/30/prime01/
 // 上記サイトを参考にしました。
 
+console.log('=====問題1=====')
 function getPrime(){
-    const num = 100;
+    let num = new Array(100);
 
-    for(let n=2; n < num; n++){ // nが2以上の時num(100)まで繰り返す。
-      let ans = judgeSosuu(0)
+    for(let i=0; i < num.length; i++){ // iが2以上の時num(100)まで繰り返す。
+      let ans = judgeSosuu(i) // judgeSosuuに素数判断したい値を渡す
+// console.log(i + 'テスト')
 
-      if (typeof ans === 'number') {
+      if (typeof ans === 'number') { // さっき代入した値を型判定する
         console.log(`${ans}は素数です`)
       }
     }
@@ -31,40 +33,39 @@ function getPrime(){
   */
  function judgeSosuu(target: number):number | undefined {
   let retNum:number | undefined
-  let n,i;
+
   // 素数判定の処理を追加する
-  for(let i=2; i<n; i++){
-        if(n%i == 0){ // n÷iの余りがなければ、
+  for(let i=2; i < target; i++){
+        if(target%i == 0){ // target(受け取った値）÷iの余りがなければ、
               break; // 終了
         }
-    if(n == i) {// nとiが同じであれば、素数
+    if(target == i) { // target(受け取った値）とiが同じであれば、素数
     }
-
+  }
   return retNum
- }
 }
 
 /*
   2. 要素数10個で値域が1～50までのランダムな値をもつnumber型の配列を作成し、素数をコンソール出力するプログラムを作成する
 */
  
-console.log('=====問題2=====')
- let n;
- const sample = makeRandomList(10,1,50)
- console.log(`素数を出力する前:${sample}`)
+//  console.log('=====問題2=====')
+//  const sample = makeRandomList(10,1,50)
+//  console.log(`素数を出力する前:${sample}`)
 
- function sampleSosu() {
- for(n=2; n < sample.length; n++){
-  let ans = judgeSosuu(0)
-  console.log(sample + 'テスト')
+//  function sampleSosu() {
+//       // let i=2; i < sample.length; i++　元
+//      for(let i=0; i < sample.length; i++){ // sampleが2以上の時は、繰り返す
+//        let ans = judgeSosuu(i)
+// // console.log(i + 'テスト')
 
-  if (typeof ans === 'number') {
-    console.log(`${ans}は素数です`)
-  }
- }
-}
+//        if (typeof ans === 'number') {
+//         console.log(`${ans}は素数です`)
+//        }
+//      }
+//  }
 
- sampleSosu();
+//  sampleSosu();
 
 
 
