68aacd15fb548a21531afc4565ae3618e2267383
TakedaAyano <a.takeda@n-s-c.co.jp>
2022-09-09 12:11:10 +0900

    課題14作成

--- src/traning14/Practice14.ts
+++ src/traning14/Practice14.ts
@@ -9,20 +9,20 @@ import { makeRandomList } from "../traning13/Common13.js"
 // https://noumenon-th.net/programming/2018/04/30/prime01/
 // 上記サイトを参考にしました。
 
-// console.log('=====問題1=====')
-// function getPrime(){
-//     let num = new Array(100);
+console.log('=====問題1=====')
+function getPrime(){
+    let num = new Array(100);
 
-//     for(let i=2; i < num.length; i++){ // iが2以上の時num(100)まで繰り返す。
-//       let ans = judgeSosuu(i) // judgeSosuuに素数判断したい値を渡す
+    for(let i=2; i < num.length; i++){ // iが2以上の時num(100)まで繰り返す。
+      let ans = judgeSosuu(i) // judgeSosuuに素数判断したい値を渡す
 
-//       if (typeof ans === 'number') { // さっき代入した値を型判定する
-//         console.log(`${ans}は素数です`)
-//       }
-//     }
-// }
+      if (typeof ans === 'number') { // さっき代入した値を型判定する
+        console.log(`${ans}は素数です`)
+      }
+    }
+}
 
-//  getPrime(); // 上記の処理、関数を呼び出す
+ getPrime(); // 上記の処理、関数を呼び出す
 
  /**
   * 素数の判断をする
@@ -36,7 +36,7 @@ import { makeRandomList } from "../traning13/Common13.js"
 
   // 素数判定の処理を追加する
   for(let i=2; i < target; i++){ // 2から渡された数まで繰り返す。
-        if(target % i == 0){ // target(受け取った値）÷iの余りがなければ、
+        if(target % i === 0){ // target(受け取った値）÷iの余りがなければ、
           return(retNum)   
     }
   }
@@ -44,22 +44,6 @@ import { makeRandomList } from "../traning13/Common13.js"
 }
 
 
-// 元
-// function judgeSosuu(target: number):number | undefined {
-//   let retNum:number | undefined
-
-//   // 素数判定の処理を追加する
-//   for(let i=2; i < target; i++){ // 2から渡された数まで繰り返す。
-//         if(target % i == 0){ // target(受け取った値）÷iの余りがなければ、
-//               break; // 終了
-//         }
-//     if(target % i === 0) { // target(受け取った値）
-//     }
-//     else if(retNum){
-//     }
-//   }
-//   return target
-// }
 
  /**
   * 素数の判断をする
@@ -91,10 +75,10 @@ import { makeRandomList } from "../traning13/Common13.js"
  console.log('=====問題2=====')
  function getSosuu() {
  const sample = makeRandomList(10,1,50)
- console.log(`素数判定前:${makeRandomList}`)
+ console.log(`素数判定前:${sample}`)
 
      for(let i=2; i < sample.length; i++){ // sampleが2以上の時は、繰り返す
-         let ans = judgeSosuu(i)
+         let ans = judgeSosuu(sample[i])
        if (typeof ans === 'number') {
         console.log(`${ans}は素数です`)
        }
